9eb7c9bb000506337fab364a85db37854dc28f1e
yiyuezhuo <yiyuezhuo@gmail.com>
2021-06-26 23:43:43 +0800

    fix package list

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-from setuptools import setup #, find_packages
+from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
@@ -7,11 +7,11 @@ with open("requirements.txt") as f:
 
 setup(
     name='IWIND-LR-TOOLS',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/yiyuezhuo/IWIND-LR-TOOLS.git',
     author='yiyuezhuo',
     author_email='yiyuezhuo@gmail.com',
     description='Tools to create, manipuate and analyze IWIND-LR input and output file.',
-    packages=["iwind_lr_tools"],    
+    packages=find_packages(exclude=("tests",)),    
     install_requires=install_requires,
 )
